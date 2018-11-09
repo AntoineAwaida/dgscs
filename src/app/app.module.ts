c3c879a0f64c2edb871fdf19d0220a59fbc877e5
Arié Selinger <arieselinger@arieselinger.home>
2018-11-09 12:39:34 +0100

    Routes + dahboard bomponent + sidenav bug (height)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material'
 
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { MissionsComponent } from './missions/missions.component';
 import { WorkpackagesComponent } from './workpackages/workpackages.component';
 import { GroupsComponent } from './groups/groups.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'missions', component: MissionsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'work-packages', component: WorkpackagesComponent },
+  { path: 'tasks', component: TasksComponent },
+  // { path : 'not-found', component : FourOhFourComponent},
+  { path: '', pathMatch : 'full', redirectTo : 'dashboard'},
+  // { path : '**', redirectTo : 'not-found'}
+]
 
 @NgModule({
   declarations: [
@@ -17,13 +31,15 @@ import { GroupsComponent } from './groups/groups.component';
     TasksComponent,
     MissionsComponent,
     WorkpackagesComponent,
-    GroupsComponent
+    GroupsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, 
     MaterialModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
