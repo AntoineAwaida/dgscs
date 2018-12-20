a8dec9626748f9bf6aee1263d2a432ff1e63088f
Arié Selinger <arieselinger@arie.local>
2018-12-20 17:09:20 +0100

    localhost

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,7 @@
-export const api = 'http://localhost:3000/api/'
-export const server = 'http://localhost:3000/'
-export const url = 'http://localhost:4200';
+// export const api = 'http://localhost:3000/api/'
+// export const server = 'http://localhost:3000/'
+// export const url = 'http://localhost:4200';
+
+export const api = 'http://cs3.cs-campus.fr:3000/api/'
+export const server = 'http://cs3.cs-campus.fr:3000/'
+export const url = 'http://cs3.cs-campus.fr:4200';
