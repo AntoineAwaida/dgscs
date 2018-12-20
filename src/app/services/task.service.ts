abc9ebbee772d1461c4303c8c6c57f686a9433c6
Arié Selinger <arieselinger@arie.local>
2018-12-20 16:54:23 +0100

    api

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,7 @@ import { Task } from '../tasks/task';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { api, server } from './../constants';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,16 @@ export class TaskService {// service qui met à jour les tâches
   constructor(private auth : AuthService, private httpClient: HttpClient, private router: Router) { }
   
   getTasks(): Observable<any> {
-    return this.httpClient.get("http://localhost:3000/api/tasks/gettasks/"+this.auth.getPayload()._id);
+    return this.httpClient.get(api+"tasks/gettasks/"+this.auth.getPayload()._id);
 
   }
 
   getTask(id : string) : Observable<any>  {
-    return this.httpClient.post("http://localhost:3000/api/tasks/gettask/"+this.auth.getPayload()._id, { taskID : id });
+    return this.httpClient.post(api+"tasks/gettask/"+this.auth.getPayload()._id, { taskID : id });
   }
 
   createTask(task){
-    this.httpClient.post("http://localhost:3000/api/tasks/createtask/", task).subscribe(
+    this.httpClient.post(api+"createtask/", task).subscribe(
       (res) => {
         console.log(res);
         this.router.navigate(['tasks']);
@@ -31,7 +32,7 @@ export class TaskService {// service qui met à jour les tâches
   }
 
   editTaskStatus(id: string, status: string) : Observable<any>  {
-    return this.httpClient.put("http://localhost:3000/api/tasks/edittaskstatus/"+this.auth.getPayload()._id, { taskID : id, status : status });
+    return this.httpClient.put(api+"tasks/edittaskstatus/"+this.auth.getPayload()._id, { taskID : id, status : status });
   }
 
 }
