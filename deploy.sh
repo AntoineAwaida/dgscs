550e4cb6cfce508d83c2439d4d0bb9a2f2503cbb
Arié Selinger <arieselinger@arie.local>
2018-12-20 17:34:47 +0100

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull
+ng build --prod
+
+
+#On supprime le contenu final
+
+sudo rm -r /var/www/html/*;
+
+#On déplacer ce qu'il y a dans la source
+sudo mv dist/dgscs/* /var/www/html
