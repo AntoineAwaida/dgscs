e0580a5adf3e32df403fa6765a560e3a0cefbe44
Arié Selinger <arieselinger@arie.home>
2018-12-26 14:08:23 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,19 +27,19 @@ import { NotFoundComponent } from './routing-error/not-found/not-found.component
 
 const appRoutes: Routes = [
   { path: 'groups', canActivate:[AuthGuardService, PendingGuardService], component: GroupsComponent, data: { title: 'Mes Groupes' }},
-  { path: 'missions', canActivate:[AuthGuardService, PendingGuardService], component: MissionsComponent, data: { title: 'Mes Work Packages' } },
+  { path: 'missions', canActivate:[AuthGuardService, PendingGuardService], component: MissionsComponent, data: { title: 'Mes Missions' } },
   { path: 'dashboard', canActivate:[AuthGuardService], component: DashboardComponent,data: { title: 'Mon Dashboard' } },
   { path: 'work-packages',  canActivate:[AuthGuardService, PendingGuardService], component: WorkpackagesComponent, data: { title: 'Mes Work Packages' }},
   { path: 'workpackage/:id', canActivate:[AuthGuardService, PendingGuardService], component: WorkpackageComponent },
 
   { path: 'register', canActivate:[AuthGuardService], component: RegisterComponent, data: { title: 'Inscription' } },
   { path: 'login', canActivate:[AuthGuardService], component: LoginComponent, data: { title: 'Connexion' } },
-  { path: 'profile', canActivate:[AuthGuardService, PendingGuardService], component: ProfileComponent, data: { title: 'Mon Profil' }},
+  { path: 'profile', canActivate:[AuthGuardService, PendingGuardService], component: ProfileComponent, data: { title: 'Mon profil' }},
   { path: 'group/:id', canActivate:[AuthGuardService, PendingGuardService], component: GroupComponent},
 
-  { path: 'not-activated', canActivate:[AuthGuardService], component: NotActivatedComponent, data: { title: 'Utilisateur Non Activé' }},
-  { path: 'not-admin', canActivate:[AuthGuardService], component: NotAdminComponent, data: { title: 'Utilisateur Non Admin' }},
-  { path: 'not-found', canActivate:[AuthGuardService], component : NotFoundComponent, data: { title: 'Page Non Trouvée' }},
+  { path: 'not-activated', canActivate:[AuthGuardService], component: NotActivatedComponent, data: { title: 'Utilisateur non activé' }},
+  { path: 'not-admin', canActivate:[AuthGuardService], component: NotAdminComponent, data: { title: 'Utilisateur non admin' }},
+  { path: 'not-found', canActivate:[AuthGuardService], component : NotFoundComponent, data: { title: 'Page non trouvée' }},
 
   { path: '', pathMatch : 'full', redirectTo : 'dashboard'},
   { path: '**', redirectTo : 'not-found'}
