c10ca62d218d4011871bcfed4649721441ab2a27
Antoine Awaida <antoine.awaida@student.ecp.fr>
2019-01-04 17:41:58 +0100

    correction bug chat

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,5 +3,5 @@
 // export const url = 'http://localhost:4200';
 
 export const api = 'http://cs3.cs-campus.fr:3000/api/'
-export const server = 'http://cs3.cs-campus.fr:3000/'
+export const server = 'http://cs3.cs-campus.fr:3000'
 export const url = 'http://cs3.cs-campus.fr';
